e1ee01ccb2002fe4b60cd6b91295f765403b4d7d
Michael Salvati <print@3daddict.com>
2018-12-17 20:09:24 -0800

    working on homepage

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <?php get_header(); ?>
 
+<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
 
+    
 
-<main role="main">
-    <section class="row">
-        <div class="col-10 offset-1">
-
-            <?php if(have_posts()) : while (have_posts()) : the_post(); ?>
-
-                <h1><?php the_title(); ?></h1>
-                <p><?php the_content(); ?></p>
-
-            <?php endwhile; else : ?>
-                <p><?php _e('Sorry, no posts matched your criteria.') ?></p>
-            <?php endif; ?>
-
-        </div>
-    </section>
-</main>
-
+<?php endwhile; else : ?>
+    <p><?php _e('Sorry, no posts matched your criteria.') ?></p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
 
-<!-- <section class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-6">Wrinkle Pro</h1>
-        <div class="container">
-            <div class='embed-responsive embed-responsive-16by9'>
-                <iframe class="embed-responsive-item" src='https://www.youtube.com/embed/GOElqG3qTAE?controls=0' frameborder='0' allowfullscreen></iframe>
-            </div>
+<!-- <section class="container-fluid">
+  <div class="row">
+    <div class="col-12 p-0">
+      <div class="jumbotron jumbotron-fluid p-0">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe
+            class="embed-responsive-item"
+            src="https://www.youtube.com/embed/GOElqG3qTAE?controls=0"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
     </div>
-    </section> -->
+  </div>
+  </section>
+  <section class="container-fluid">
+  <div class="row">
+      <div class="col-10 offset-1 mt-4 mb-4">
+        <h1>Introducing the Wrinkle Pro</h1>
+        <p>It wraps around your ear, and separates the front part of your face from smashing into the pillow protecting delicate skin under your eyes from experiencing hours of damaging stress, and pressure, that causes deeper wrinkles, and crevices. The sooner you start using the wrinkle stopping face cloud along with your favorite wrinkle diminishing cream, the sooner you'll have wonderful nights of healing sleep, and our project hopes, that through you, a bigger healing will take place.</p>
+      </div>
+    </div>
+</div>
+</section> -->
+
